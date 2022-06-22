44efc24ff480f2054776d4830609d16543482e40
Cody-Wise <cody@sharptackmedia.com>
2022-06-22 12:38:49 -0700

    Finished Fibbonacci Function

--- index.js
+++ index.js
@@ -45,3 +45,14 @@ function addFirst(element) {
 
 const addOrange = addFirst('cat');
 console.log(addOrange(['bird', 'dog', 'lizard']));
+
+//Fibonacci Function
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(7));
