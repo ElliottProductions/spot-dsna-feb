6906c141ec9ebb5546c8a676bb1d11aa74d518b6
Cody-Wise <cody@sharptackmedia.com>
2022-06-22 10:15:14 -0700

    Working on filter function

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 function map(arr, callback) {}
 const array = [1, 6, 5];
-map(array, (numba) => (numba**2));
+map(array, (numba) => numba ** 2);
 console.log(array);
+
+map([1, 6, 5], (n) => n ** 2);
+
+// Write a function `filter` that takes an array and a predicate callback function:
+// - it returns a _new_ array
+// - it calls the callback function with each element of the passed array and conditionally adds the item to the new array if the predicate function returns a truthy value
+
+function filter(arr, predicate) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (predicate(arr[i])) {
+      arr2.push(arr[i]);
+    }
+  }
+}
